7cff4450fd7ddb4b5e1365df3d2480270a043ee3
charles.gallagher07 <charles.gallagher07@gmail.com>
2023-03-21 21:03:48 -0500

    some work on read_data

--- hetrogenous_salvo_read_data.py
+++ hetrogenous_salvo_read_data.py
@@ -3,4 +3,18 @@ import pandas as pd
 path = "hetrogeneous_salvo_data_input_tool.xlsx"
 
 group = pd.read_excel(path, sheet_name= 0, index_col=[0], engine='openpyxl')
-print(group)
+group = [x for _, x in group.groupby('Formations')]
+
+for x in range(len(group)):
+    temp = group[x]
+    n = group[x].shape[1] # num enemy targets
+    unit_dict = {}
+    num_const = 4
+    num_array = 12
+    for i in range(num_const):
+        unit_dict[temp.iloc[i,0]] = temp.iloc[i,1]
+        
+    for i in range(num_const, num_array):
+        unit_dict[temp.iloc[i,0]] = temp.iloc[i,2:n].to_numpy() # not complete this is wrong
+
+    print(unit_dict)
