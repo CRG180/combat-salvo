dca311aea4d60aff277ca51dc6d25c5b804bbda4
charles.gallagher07 <charles.gallagher07@gmail.com>
2023-04-03 07:07:55 -0500

    data class used

--- hetrogeneous_salvo_model/hetrogeneous_salvo_model.py
+++ hetrogeneous_salvo_model/hetrogeneous_salvo_model.py
@@ -1,25 +1,88 @@
 from hetrogenous_salvo_read_data import read_input_file
+from dataclasses import dataclass
 import numpy as np
 
-
-class Unit:
-    def __init__(self,unit_dict):
-        self.formation = unit_dict["formation"]
-        self.type = unit_dict["type"]
-        self.mgrs = unit_dict["mgrs"]
-        self.range = unit_dict["range"]
-        self.num_units = unit_dict["num_units"]
-        self.num_missiles_off = unit_dict["num_missiles_off"]
-        self.num_missiles_def = unit_dict["num_missiles_def"]
-        self.aimed_offense  = unit_dict["aimed_offense"]
-        self.fraction_engage = unit_dict["fraction_engage"]
-        self.defense_capability = unit_dict["defense_capability "]
-        self.staying_power = unit_dict["staying_power"]
-        self.scouting = unit_dict["scouting"]
-        self.alertness = unit_dict["alertness"]
-        self.training = unit_dict["training"]
-        self.distraction = unit_dict["distraction"]
+def check_value_0_1(input_array, attribute ,formation):
+    for i in input_array:
+        if i > 1 or i < 0:
+            raise ValueError(f"{attribute} value for {formation} is incorrect with value as {i}")
+    return None
     
+
+
+@dataclass
+class Unit:
+    unit_dict: dict
+    
+    @property
+    def formation(self):
+        return self.unit_dict["formation"]
+    
+    @property
+    def unit_type(self):
+        return self.unit_dict["type"]
+    
+    @property
+    def mgrs(self):
+        return self.unit_dict["mgrs"]
+    
+    @property
+    def weapon_range(self):
+        return self.unit_dict["range"]
+    
+    @property
+    def num_units(self):
+        return self.unit_dict["num_units"]
+    
+    @property
+    def num_missiles_off(self):
+        return self.unit_dict["num_missiles_off"]
+    
+    @property
+    def num_missiles_def(self):
+        return self.unit_dict["num_missiles_def"]
+    
+    @property
+    def aimed_offense(self):
+        return self.unit_dict["aimed_offense"]
+    
+    @property
+    def aimed_offense(self):
+        return self.unit_dict["aimed_offense"]
+    
+    @property
+    def aimed_offense(self):
+        return self.unit_dict["aimed_offense"]
+
+    @property
+    def fraction_engage(self):
+        return self.unit_dict["fraction_engage"]
+
+    @property
+    def defense_capability(self):
+        return self.unit_dict["defense_capability"]
+
+    @property
+    def staying_power(self):
+        return self.unit_dict["staying_power"]
+    
+    @property
+    def scouting(self):
+        check_value_0_1(self.unit_dict["scouting"],self.formation)
+        return self.unit_dict["scouting"]
+    
+    @property
+    def alertness(self):
+        return self.unit_dict["alertness"]
+
+    @property
+    def training(self):
+        return self.unit_dict["training"]
+    
+    @property
+    def distraction(self):
+        return self.unit_dict["distraction"]
+        
     @property
     def offense_shots_available(self):
         return self.num_units * self.num_missiles_off
@@ -88,6 +151,9 @@ class Engagement:
         self.red_force = red_force
         self.offensive_side = offensive_side
         
+    def decrement_attrition_values(self):
+        pass
+        
     def salvo(self):
         off = np.matmul(self.blue_force.offense_matrix,\
             self.blue_force.formation_vec)
@@ -95,8 +161,6 @@ class Engagement:
              self.red_force.formation_vec )
         return (off - defen)
         
-    def decrement_attrition_values(self):
-        pass
     
     def iter_salvo(self):
         pass
@@ -106,6 +170,8 @@ if __name__ == "__main__":
     data_b = read_input_file(side=0)
     a = BattleGroup(data_a, battle_group_name="blue")
     b = BattleGroup(data_b,battle_group_name="red")
+    print(vars(Unit))
+    
     print("b offense")
     print(b.offense_matrix)
     print("b formation_vec")
@@ -115,4 +181,5 @@ if __name__ == "__main__":
     #print(b.units[0].offense_vector)
     e = Engagement(a,b)
     print(e.salvo())
+    
     
